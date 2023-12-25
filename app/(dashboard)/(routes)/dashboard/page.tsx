a6915eb435ccb3b73004888b117e83ec66056d65
Hassaan Muhammad <hassaans299@gmail.com>
2023-12-25 12:58:48 -0800

    clerk auth done

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from "@clerk/nextjs"
 
 
 const DashBoardPage=()=> {
   return(
+    <div>
     <p>Dashboard page (protected)</p>
+    <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
 export default DashBoardPage
