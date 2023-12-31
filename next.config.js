6034c370d782c10e1b562d07acde0de23be4b8f7
Hassaan Muhammad <hassaans299@gmail.com>
2023-12-31 07:51:46 -0800

    ready to deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // images:{
+    //     domains:[
+    //         "oaidalleapiprodscus.blob.core.windows.ne"
+    //     ]
+    // }
+}
 
 module.exports = nextConfig
