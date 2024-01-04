1b96d0955f3b3ac1a56b6f27a94e8d2c5c7cec6e
Hassaan Muhammad <hassaans299@gmail.com>
2024-01-04 12:36:44 -0800

    image generation is done

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // images:{
-    //     domains:[
-    //         "oaidalleapiprodscus.blob.core.windows.ne"
-    //     ]
-    // }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
